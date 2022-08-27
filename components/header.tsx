64769f5712a465d04ca0fd89dfc2b9f08f17bd2d
ritvij14 <ritvij2001@gmail.com>
2022-08-28 04:09:42 +0530

    mega-revamp part 1 done

--- components/header.tsx
+++ components/header.tsx
@@ -1,43 +1,42 @@
-import { Flex, Text, useColorMode } from "@chakra-ui/react";
+import { Flex, Image, Text, useColorMode } from "@chakra-ui/react";
 import React from "react";
 import "@fontsource/source-code-pro";
 import ChakraBox from "./animate_box";
+import ColorModeToggle from "./themeToggle";
 
 const Header = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
-    <Flex
-      direction="row"
-      w="100%"
-      justify="space-between"
-      alignItems="center"
-      backgroundColor={colorMode == "light" ? "white" : "#1A202C"}
-      px="6rem"
-      py="2rem"
-      boxShadow="lg"
+    <ChakraBox
+      initial={{ opacity: 0, y: 10 }}
+      animate={{ opacity: 1, y: 0 }}
+      // @ts-ignore no problem in operation, although type error appears.
+      transition={{
+        duration: 0.2,
+        ease: "easeIn",
+      }}
     >
-      <ChakraBox
-        initial={{ opacity: 0, y: 10 }}
-        animate={{ opacity: 1, y: 0 }}
-        // @ts-ignore no problem in operation, although type error appears.
-        transition={{
-          duration: 0.5,
-          ease: "easeIn",
-        }}
+      <Flex
+        direction="row"
+        w="100%"
+        justify="space-between"
+        alignItems="center"
+        px="4rem"
+        py="2rem"
       >
-        <Text
-          variant="unstyled"
-          fontSize="2xl"
-          fontWeight="medium"
-          fontFamily="nav-logo"
-        >
-          {"<>ritvij</>"}
-        </Text>
-      </ChakraBox>
+        <Image
+          src={
+            colorMode == "dark"
+              ? "/images/logo_blue.svg"
+              : "/images/logo_black.svg"
+          }
+          alt="ritvij_memoji"
+        />
 
-      {/* <ColorModeToggle /> */}
-    </Flex>
+        <ColorModeToggle />
+      </Flex>
+    </ChakraBox>
   );
 };
 
