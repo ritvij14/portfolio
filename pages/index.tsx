0f4d9736f1d5471795166443a85b637d3346897d
ritvij14 <ritvij2001@gmail.com>
2022-08-29 00:12:24 +0530

    added website title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box, useColorModeValue } from "@chakra-ui/react";
 import type { NextPage } from "next";
+import Head from "next/head";
 import About from "../components/about";
 import Footer from "../components/footer";
 import Header from "../components/header";
@@ -8,6 +9,11 @@ import Intro from "../components/intro";
 const Home: NextPage = () => {
   return (
     <Box>
+      <Head>
+        <title>Ritvij Kumar Sharma</title>
+        <link rel="icon" href="/images/logo_blue.svg" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Header />
       <Intro />
       <About />
